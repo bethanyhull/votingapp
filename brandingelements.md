9c43937a3fc9a70e1aa842e230e33956f8af4d07
Bethany Hull <H-Sfamily@Bethanys-iMac.PK5001Z>
2018-11-03 08:20:55 -0600

    styling and messing around with button in js

--- brandingelements.md
+++ brandingelements.md
@@ -6,6 +6,7 @@ blue rgb(12, 59, 93)
 grey #605E5E;
 
 
+
 font-family: futura-lt-w01-book,sans-serif;
 main: font: normal normal normal 25px/1.4em avenir-lt-w01_35-light1475496,sans-serif;
 titles: font: normal normal normal 60px/1.4em dinneuzeitgroteskltw01-_812426,sans-serif;
--- script.js
+++ script.js
@@ -1,240 +1,31 @@
-/*********************
- *	Helpers Code
- ********************/
-/**
- *  @function   DOMReady
- *
- *  @param callback
- *  @param element
- *  @param listener
- *  @returns {*}
- *  @constructor
- */
-const DOMReady = ((
-  callback  = () => {},
-  element   = document,
-  listener  = 'addEventListener'
-) => {
-  return (element[listener]) ? element[listener]('DOMContentLoaded', callback) : window.attachEvent('onload', callback);
-});
 
+var slides = $('.slide');
 
-/*********************
- *	Application Code
- ********************/
-/**
- *  @class  PureSlider
- */
-class PureSlider {
-  /**
-   *  @constructor
-   *
-   *  @param element
-   *  @param options
-   */
-  constructor (element, options = {}) {
-    this.el       = document.querySelector(element);
-    this.options  = Object.assign({
-      'links':          '.js-slider__nav .js-slider__slide-nav-link',
-      'wrapper':        '.js-slider__slide-wrapper',
-      'prev':           '.js-slider__prev',
-      'next':           '.js-slider__next',
-      'slide':          '.js-slider__slide',
-      'slide-caption':  '.js-slider__slide-caption',
-      'slide-nav':      '.js-slider__slide-nav',
-      'slide-nav-link': '.js-slider__slide-nav-link'
-    }, options);
+slides.first().before(slides.last());
+setInterval(show,8000);
 
-    this.init();
-  }
-  /**
-   *  @function init
-   *
-   *  @public
-   */
-  init () {
-    this.links    = this.el.querySelectorAll(this.options.links);
-    this.wrapper  = this.el.querySelector(this.options.wrapper);
+function show(){
+  slides = $('.slide');
+  var activeSlide = $('.active');
 
-    this.prev     = this.el.querySelector(this.options.prev);
-    this.next     = this.el.querySelector(this.options.next);
+  slides.last().after(slides.first());
 
-    /**
-     *  Init Default state
-     */
-    this._moveSlide(
-      this.links[0]
-    );
-    /**
-     *  Init Navigation
-     */
-    this._navigate();
-  }
-  /**
-   *  @function _navigate
-   *
-   *  @private
-   */
-  _navigate () {
-    for (let i = 0, link; i < this.links.length; ++i) {
-      link = this.links[i];
-
-      this._slideByNav(link);
-    }
-
-    this._slideByButton(
-      this.prev,
-      -1
-    );
-    this._slideByButton(
-      this.next,
-      1
-    );
-  }
-  /**
-   *  @function _slideByNav
-   *
-   *  @param element
-   *  @private
-   */
-  _slideByNav (element) {
-    let self        = this,
-        currentLink;
-
-    /**
-     *  It is magic of BabelJS
-     */
-    element.addEventListener('click', function (e) {
-      currentLink   = this;
-
-      e.preventDefault();
-
-      self._moveSlide(
-        currentLink
-      );
-    }, false);
-  }
-  /**
-   *  @function _slideByButton
-   *
-   *  @param element
-   *  @param arg
-   *  @private
-   */
-  _slideByButton (element, arg = null) {
-    let self        = this,
-        currentSlide;
-
-    element.addEventListener('click', (e) => {
-      currentSlide  = self.el.querySelector(
-        this.options.slide + ' ' + this.options['slide-caption'] + '--current'
-      ).parentNode;
-
-      e.preventDefault();
-
-      self._moveSlide(
-        currentSlide,
-        arg
-      );
-    }, false);
-  }
-  /**
-   *  @function _moveSlide
-   *
-   *  @param element
-   *  @param arg
-   *  @private
-   */
-  _moveSlide (element, arg = null) {
-    let self      = this,
-        currentIndex,
-        currentLink,
-        currentSlide;
-
-    currentIndex  = parseInt(element.getAttribute('data-slide'), 10);
-    currentLink   = self.el.querySelector(
-      this.options['slide-nav'] + ':nth-child(' + parseInt(currentIndex + arg, 10) + ')'
-    );
-    currentSlide  = self.el.querySelector(
-      this.options.slide + ':nth-child(' + parseInt(currentIndex + arg, 10) + ')'
-    );
-
-    if (!currentSlide) {
-      return;
-    }
-
-    self._setCurrentLink(currentLink);
-
-    self.wrapper.style.left = '-' + currentSlide.offsetLeft + 'px';
-    self._animateOfCaption(currentSlide);
-  }
-  /**
-   *  @function _animateOfCaption
-   *
-   *  @param slide
-   *  @private
-   */
-  _animateOfCaption (slide) {
-    let captionOfSlide      = slide.querySelector(this.options['slide-caption']),
-        captions            = slide.parentNode.querySelectorAll(this.options['slide-caption']),
-        captionToggleClass  = this.options['slide-caption'].substring(1) + '--current';
-
-    this._toggleClass(
-      captionOfSlide,
-      captions,
-      captionToggleClass
-    );
-  }
-  /**
-   *  @function _setCurrentLink
-   *
-   *  @param link
-   *  @private
-   */
-  _setCurrentLink (link) {
-    let childrenOfNav   = link.querySelector(this.options['slide-nav-link']),
-        links           = this.links,
-        linkToggleClass = this.options['slide-nav'].substring(1) + '--current';
-
-    this._toggleClass(
-      childrenOfNav,
-      links,
-      linkToggleClass
-    );
-  }
-  /**
-   *  @function _toggleClass
-   *
-   *  @param currentElement
-   *  @param allElements
-   *  @param toggleOfClass
-   *  @private
-   */
-  _toggleClass (currentElement, allElements, toggleOfClass) {
-    for (let i = 0, currentItem; i < allElements.length; ++i) {
-      currentItem = allElements[i];
-
-      if (currentItem !== currentElement) {
-        currentItem.classList.remove(toggleOfClass);
-      }
-    }
-
-    currentElement.classList.add(toggleOfClass);
-  }
+  activeSlide.removeClass('active').next('.slide').addClass('active');
 }
 
+var slide1 = document.getElementByClass('s1');
+var slide2 = document.getElementByClass('s2');
+var slide3 = document.getElementByClass('s3');
 
-/**
- *  @function   readyFunction
- *
- *  @type {Function}
- */
-const readyFunction = (() => {
-  return new PureSlider('.pure-slider');
-});
+slide1.addEventListener("click", butChange());
 
+function butChange() {
+  if(button.innerHTML = 'Select an organization') {
+    button.innerHTML = 'Vote for Organization 1';
+  }
+  else () {
+    button.innerHTML - 'Select an organization';
+  }
 
-/**
- *  Launcher
- */
-DOMReady(readyFunction);
+  }
+}
